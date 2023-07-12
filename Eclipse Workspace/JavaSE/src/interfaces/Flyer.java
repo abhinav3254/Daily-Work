5a5d3e58928558c38d53073ad15594d140fdd3bf
Abhinav Jha <97444941+abhinav3254@users.noreply.github.com>
2023-07-12 14:58:03 +0530

    assignment 12 July 2023

---
+++ Eclipse Workspace/JavaSE/src/interfaces/Flyer.java
@@ -0,0 +1,5 @@
+package interfaces;
+
+public interface Flyer {
+	public void fly();
+}
---
+++ Eclipse Workspace/JavaSE/src/interfaces/Main.java
@@ -0,0 +1,9 @@
+package interfaces;
+
+public class Main {
+	public static void main(String[] args) {
+		SuperMan superMan = new SuperMan("Hero", 25);
+		superMan.printDetails();
+		superMan.fly();
+	}
+}

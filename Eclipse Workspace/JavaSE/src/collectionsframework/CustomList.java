63cd875c27c8bd59b7db437bd32aa921fa886727
Abhinav Jha <97444941+abhinav3254@users.noreply.github.com>
2023-07-20 13:27:33 +0530

    update employee

--- Eclipse Workspace/JavaSE/src/collectionsframework/CustomList.java
+++ Eclipse Workspace/JavaSE/src/collectionsframework/CustomList.java
@@ -2,17 +2,28 @@ package collectionsframework;
 
 import java.util.LinkedList;
 
+import genrics.Employee;
+
 public class CustomList {
+	
 	public static void main(String[] abhinav) {
 		abhinav = new String[2];
 		abhinav[0] = "args";
+//		
+//		LinkedList<String> list = new LinkedList<String>();
+//		
+//		list.add("WELCOME");
+//		list.add("BANANA");
+//		list.add("HELLO");
+//		list.add("APPLE");
+//		
+//		System.out.println(list.toString());
 		
-		LinkedList<String> list = new LinkedList<String>();
-		
-		list.add("WELCOME");
-		list.add("BANANA");
-		list.add("HELLO");
-		list.add("APPLE");
+		LinkedList<Employee> list = new LinkedList<Employee>();
+		list.add(new Employee(102, "Bhavesh", 15000));
+		list.add(new Employee(104, "Utkarsh", 14000));
+		list.add(new Employee(106, "Anand", 20000));
+		list.add(new Employee(108, "Gautam", 22222));
 		
 		System.out.println(list.toString());
 		

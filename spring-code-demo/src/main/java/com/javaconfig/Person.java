efc734944f46c7d68bb99dfe6c424da91e230cc9
Abhinav Jha <97444941+abhinav3254@users.noreply.github.com>
2023-08-01 17:24:29 +0530

    using java setting up injection

---
+++ spring-code-demo/src/main/java/com/javaconfig/Person.java
@@ -0,0 +1,48 @@
+package com.javaconfig;
+
+
+
+public class Person {
+	private int ssn;
+	private String name;
+	private int age;
+	private Address address;
+	public Person() {
+		super();
+	}
+	public Person(int ssn, String name, int age, Address address) {
+		super();
+		this.ssn = ssn;
+		this.name = name;
+		this.age = age;
+		this.address = address;
+	}
+	public int getSsn() {
+		return ssn;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setSsn(int ssn) {
+		this.ssn = ssn;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return "Person [ssn=" + ssn + ", name=" + name + ", age=" + age + ", address=" + address + "]";
+	}
+}
